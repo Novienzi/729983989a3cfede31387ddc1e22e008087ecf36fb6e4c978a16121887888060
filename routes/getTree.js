44cc13f9269fc840fa3bd1d77d4efd558da7924d
novienzi <novienghi@gmail.com>
2021-06-28 14:34:09 +0700

    update get tree

--- routes/getTree.js
+++ routes/getTree.js
@@ -3,9 +3,9 @@ const app = express.Router()
 const db = require('../controller/getTree')
 
 app.get('/get/tree', async (req, res, next) => {
-    const getTree = await db.get_tree(req.params.movie_id)
+    const getTree = await db.get_tree()
         .catch((err) => next(err))
-    if (getTree.length > 0) {
+    if (getTree) {
         res.status(200).send(getTree);
     } else {
         res.status(404).send("Data is not found");
